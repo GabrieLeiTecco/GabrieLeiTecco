0bd8c85bd8d494fac34ce8316e1ef03d562a7445
Gabriel Leite Tecco <111099300+GabrieLeiTecco@users.noreply.github.com>
2024-09-30 20:43:58 -0300

    Add files via upload

--- Site Gabriel/src/main.js
+++ Site Gabriel/src/main.js
@@ -35,21 +35,10 @@ var fonts = [
 ]
 
 var cores = [
-    "#FF0000", "#008000", "#0000FF", "#FFFF00", 
-    "#00FFFF", "#FF00FF", "#000000", "#FFFFFF", 
-    "#808080", "#FFA500", "#800080", "#A52A2A", 
-    "#FFC0CB", "#00FF00", "#808000", "#000080", 
-    "#008080", "#800000", "#C0C0C0", "#FFD700", 
-    "#FF7F50", "#40E0D0", "#4B0082", "#EE82EE", 
-    "#FFDAB9", "#E6E6FA", "#FA8072", "#F0E68C", 
-    "#D2691E", "#708090", "#6A5ACD", "#3CB371", 
-    "#4169E1", "#9932CC", "#B22222", "#EEE8AA", 
-    "#F5DEB3", "#9370DB", "#E9967A", "#20B2AA", 
-    "#A0522D", "#F0FFF0", "#F08080", "#DA70D6", 
-    "#B8860B", "#FF6347", "#FFE4E1", "#87CEFA", 
-    "#C71585", "#00FF7F", "#6495ED", "#FFA07A"
+    "#FFB3BA", "#FFDFBA", "#FFFABA", "#BAFFC9", "#BAE1FF", "#FFD1FF", "#FFABAB", "#FFEC40", "#C4E538",
+    "#38B6E6", "#FF6F61", "#F5B041", "#F7CAC9", "#D5AAFF", "#A2DFF7", "#FF8A5B", "#FFB74D", "#B9FBC0",
+    "#FF9A8B", "#FFE156", "#FF6F91"
 ]
-
 
 function sortearTitulo() {
     let num_nome = Math.floor(Math.random()*(nomes.length - 0) + 0)
@@ -63,15 +52,16 @@ function sortearTitulo() {
     titulo.style.color = cores[num_cor]
 }
 
-window.onclick = function (){
-    if(!event.target.matches('.btn-menu')){
-        var menuDropdown = document.getElementsByClassName('conteudo-menu')
-        var i 
-        for (i = 0; i < menuDropdown.length; i++) {
-            var menuAberto = menuDropdown[i]
-            if (menuAberto.classList.contains('show') ){
-                menuAberto.classList.remove('show')
-            }
+function menu(){
+    var menuDropdown = document.getElementsByClassName('conteudo-menu')
+    var i 
+    for (i = 0; i < menuDropdown.length; i++) {
+        var menuAberto = menuDropdown[i]
+        if (menuAberto.classList.contains('show') ){
+            menuAberto.classList.remove('show')
+        }else{
+            menuAberto.classList.add('show')
         }
     }
 }
+
